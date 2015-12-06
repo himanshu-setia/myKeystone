95c029892df87acb1a41a62d996ac8f6503134b0
roopali8 <roopali8.rawat@gmail.com>
2015-12-06 21:26:52 +0000

    unit test cases for attach/detach policies

--- keystone/jio_policy/controllers.py
+++ keystone/jio_policy/controllers.py
@@ -33,8 +33,6 @@ class JioPolicyV3(controller.V3Controller):
     #@controller.jio_policy_filterprotected(args='Policy')
     @validation.validated(schema.policy_create, 'policy')
     def create_policy(self, context, policy):
-        import pdb;
-        pdb.set_trace()
         policy_id = uuid.uuid4().hex
         try:
             project_id = context['environment']['KEYSTONE_AUTH_CONTEXT'][
